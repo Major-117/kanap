50cc17048be9f7763df3b2b2bec6831d97a1f22a
NASSBOKO <nassboko54@outlook.com>
2023-02-09 18:03:26 +0100

    kanap.V0

--- front/js/script.js
+++ front/js/script.js
@@ -11,14 +11,16 @@ requete.onload = function(){
     if (requete.readyState === XMLHttpRequest.DONE){
         if (requete.status === 200){
             let reponse = requete.response;
-            let Kanap_Sinope  = reponse[0];
-            let Kanap_Cyllene = reponse[1];
-            let Kanap_Calyce  = reponse[2];
-            let Kanap_Autonoe = reponse[3];
-            let Kanap_Eurydome = reponse[4];
+            let Kanap_Sinope    = reponse[0];
+            let Kanap_Cyllene   = reponse[1];
+            let Kanap_Calyce    = reponse[2];
+            let Kanap_Autonoe   = reponse[3];
+            let Kanap_Eurydome  = reponse[4];
+            let Kanap_Helice    = reponse[5];
+            let Kanap_Thyone    = reponse[6];
+            let Kanap_orthosie  = reponse[7];
 
-
-            console.log(Kanap_Sinope,Kanap_Cyllene);
+            console.log(Kanap_Sinope,Kanap_Cyllene,Kanap_Calyce,Kanap_Autonoe,Kanap_Eurydome,Kanap_Helice,Kanap_Thyone,Kanap_orthosie);
 
         }
         else{
